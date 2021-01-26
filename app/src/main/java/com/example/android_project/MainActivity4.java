e4a041a05b13657c5267a63f425de8d3a87bbe5f
making <miguelmape7@gmail.com>
2021-01-26 18:40:42 +0100

    firt

--- app/src/main/java/com/example/android_project/MainActivity4.java
+++ app/src/main/java/com/example/android_project/MainActivity4.java
@@ -15,26 +15,16 @@ import java.util.ArrayList;
 import Entidades.Plato;
 import utilidades.Utilidades;
 
-public class MainActivity4 extends AppCompatActivity {
-    private ListView listViewPlatos;
-    private ArrayList<String>lista_informacion;
-    private ArrayList<Plato>lista_platos;
-    private ConexionSQLiteHelper conn;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import utilidades.Utilidades;
 
 public class MainActivity4 extends AppCompatActivity {
-
+    private ListView listViewPlatos;
+    private ArrayList<String>lista_informacion;
+    private ArrayList<Plato>lista_platos;
     private EditText ed_nombre;
     private EditText ed_password;
     private TextView tv_1;
